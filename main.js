54e8aef30d7d96594fd5c44d9113a4c0b0fc8bcb
VB <115148657+vidushi711@users.noreply.github.com>
2025-04-28 02:38:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NTY4MWEyMC01NDg2LTRjYWEtODExOS0zMjQ3NGNiNDZkMmMiLCJpZCI6MjYwMTgwLCJpYXQiOjE3MzMzNjE1MDN9.OIim-jXbq3AfbnBU2rS2SGRG4DKwO88JR_2ycGIEk8w';
 
 async function startViewer() {
+    const terrain = await Cesium.CesiumTerrainProvider.fromIonAssetId(1);
     const viewer = new Cesium.Viewer('cesiumContainer', {
-        terrainProvider: await Cesium.CesiumTerrainProvider.fromIonAssetId(1),
+        terrainProvider: terrain,
         timeline: true,
         animation: true,
         shouldAnimate: true
@@ -12,9 +13,8 @@ async function startViewer() {
     const bkTileset = await Cesium.Cesium3DTileset.fromIonAssetId(2955578);
     viewer.scene.primitives.add(bkTileset);
 
-    // Zoom to your building
+    // Zoom to building
     viewer.zoomTo(bkTileset);
 }
 
-// Start the viewer
 startViewer();
