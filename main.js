e36603d594c8f7e3c39bd34d676dea9c496b1941
VB <115148657+vidushi711@users.noreply.github.com>
2025-04-28 02:22:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-// Your Access Token here:
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NTY4MWEyMC01NDg2LTRjYWEtODExOS0zMjQ3NGNiNDZkMmMiLCJpZCI6MjYwMTgwLCJpYXQiOjE3MzMzNjE1MDN9.OIim-jXbq3AfbnBU2rS2SGRG4DKwO88JR_2ycGIEk8w';
 
-const viewer = new Cesium.Viewer('cesiumContainer', {
-    terrainProvider: Cesium.createWorldTerrain(),
-    timeline: true,
-    animation: true,
-    shouldAnimate: true
-});
+async function startViewer() {
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+        terrainProvider: Cesium.createWorldTerrain(),
+        timeline: true,
+        animation: true,
+        shouldAnimate: true
+    });
 
-// Load your BK building model
-const bkTileset = viewer.scene.primitives.add(
-    await Cesium.Cesium3DTileset.fromIonAssetId(2955578)
-);
+    // Load your BK building model
+    const bkTileset = await Cesium.Cesium3DTileset.fromIonAssetId(2955578);
+    viewer.scene.primitives.add(bkTileset);
 
-// Zoom to your building
-viewer.zoomTo(bkTileset);
+    // Zoom to your building
+    viewer.zoomTo(bkTileset);
+}
+
+// Start the viewer
+startViewer();
