5553fe475b41c36913996b75aab315677111585d
VB <115148657+vidushi711@users.noreply.github.com>
2025-04-28 02:17:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+// Your Access Token here:
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NTY4MWEyMC01NDg2LTRjYWEtODExOS0zMjQ3NGNiNDZkMmMiLCJpZCI6MjYwMTgwLCJpYXQiOjE3MzMzNjE1MDN9.OIim-jXbq3AfbnBU2rS2SGRG4DKwO88JR_2ycGIEk8w';
 
+const viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: Cesium.createWorldTerrain(),
+    timeline: true,
+    animation: true,
+    shouldAnimate: true
+});
+
+// Load your BK building model
+const bkTileset = viewer.scene.primitives.add(
+    await Cesium.Cesium3DTileset.fromIonAssetId(2955578)
+);
+
+// Zoom to your building
+viewer.zoomTo(bkTileset);
